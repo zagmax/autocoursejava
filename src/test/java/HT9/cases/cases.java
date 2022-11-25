b212fe27e2c184052f64c28a41b2931a7a63ddd5
Maksym Zahreba <maksym_zahreba@epam.com>
2022-11-25 12:51:11 +0200

    HT9 null issue

--- src/test/java/HT9/cases/cases.java
+++ src/test/java/HT9/cases/cases.java
@@ -25,32 +25,32 @@ public class cases {
     PageFactory pageFactory;
 
     @BeforeClass
-    public void setDriver() {
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        pageFactory = new PageFactory(driver);
-        basePage = pageFactory.getBasePage();
-
+    public static void setDriver() {
     }
 
     @Test
     public void test1() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.bookdepository.com/");
+        pageFactory = new PageFactory(driver);
+        basePage = pageFactory.getBasePage();
         homePage = pageFactory.getHomePage();
         basePage.waitForPageLoadComplete(DEFAULT_WAITING_TIME);
         //Actions actions = new Actions(driver);
         String nameOfBook = "Transfer Activity Book Unicorns";
-        basePage.waitFor(ExpectedConditions.visibilityOf(homePage.getSearchInputBox()));
+        //basePage.waitFor(ExpectedConditions.visibilityOf(homePage.getSearchInputBox()));
         homePage.getSearchInputBox().sendKeys("camilla");
         homePage.getSearchButton().click();
         basePage.waitForPageLoadComplete(DEFAULT_WAITING_TIME);
-        List<WebElement> searchResult = driver.findElements(By.className("book-item")).stream().filter(book-> book.getText().contains(nameOfBook)).toList();
+        List<WebElement> searchResult = driver.findElements(By.className("book-item")).stream().filter(book -> book.getText().contains(nameOfBook)).toList();
 
-        Assert.assertTrue("no book was found",searchResult.size()>0);
+        Assert.assertTrue("no book was found", searchResult.size() > 0);
+        driver.quit();
+        driver = null;
     }
 
     @AfterClass
-    public void tearDown() {
-        driver.quit();
-        driver = null;
+    public static void tearDown() {
     }
 }
