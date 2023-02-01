c809281cacb38cd62e80102ca06870b1ea57a72c
Maksym Zahreba <maksym_zahreba@epam.com>
2023-02-01 12:15:32 +0200

    regex for address

---
+++ src/main/java/ht12/Check.java
@@ -0,0 +1,8 @@
+package ht12;
+
+public class Check {
+    public static void main(String[] args) {
+        RegexTask regexTask = new RegexTask();
+        System.out.println(regexTask.getAddress());
+    }
+}
--- src/main/java/ht12/RegexTask.java
+++ src/main/java/ht12/RegexTask.java
@@ -1,5 +1,9 @@
 package ht12;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class RegexTask {
     public boolean matchStringToRegex(String text, String regex) {
         return (text.matches(regex));
@@ -14,4 +18,17 @@ public class RegexTask {
     public boolean isStringBankCardNumber(String number) {
         return (number.matches("\\d{16}"));
     }
+
+    private WebDriver driver = new ChromeDriver();
+
+    public String getAddress(){
+        driver.get("https://www.epam.com/about/who-we-are/contact");
+        String address=driver.findElement(By.cssSelector("b:first-child span.font-size-22:first-child")).getText();
+        driver.quit();
+        if(address.matches("\\d+\\ [A-Za-z\\ ]+\\ •\\ [A-Za-z\\ ]+\\d+,\\n[A-Za-z\\ ]+,\\ \\w+\\ \\d+\\ •\\ \\w+")){
+            return address;
+        }
+        else return "Valid Address info isn't found";
+    }
+
 }
